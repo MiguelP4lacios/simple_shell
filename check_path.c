195d0fab5d1534b317f1d37e543a585ed0ee7a74
Manuelpv17 <manuelpv17@outlook.com>
2020-04-16 14:46:38 -0500

    More logic for path

--- check_path.c
+++ check_path.c
@@ -4,7 +4,7 @@
  * check_path - check if path + input is exec
  * @exec: path + input
  * @head: head of list of path
- * @input_user: input user - first word
+ * @in: input user - first word
  * @numwords: number of words input
  * Return: 0 not found
  * 1 (path + input) is executable --> free in execute_func
@@ -12,30 +12,30 @@
  * 3 (path + input) || (input) exist but not executable
  * 4 Do NOTHING
  */
-int check_path(char **exec, list_path *head, char *input_user, size_t numwords, ssize_t *status)
+int check_path(char **exec, list_path *head,
+	       char *in, size_t numwords, ssize_t *status)
 {
 	size_t i;
 	struct stat sb;
 
 	if (numwords == 0)
 		return (0);
-
-	if (_strcmp(input_user, ".") == 0)
+	if (_strcmp(in, ".") == 0)
 	{
 		*status = 127;
 		return (4);
 	}
-	else if (_strcmp(input_user, "..") == 0)
+	else if (_strcmp(in, "..") == 0)
 	{
 		*status = 127;
 		return (3);
 	}
-	for (i = 0; head != NULL; i++)
+	for (i = 0; head != NULL && in[0] != '/'; i++)
 	{
-		*exec = str_concat(head->dir_path, input_user);
-		if (stat(*exec, &sb) == 0 && sb.st_mode & S_IXUSR && !S_ISDIR(sb.st_mode) && input_user[0] != '/')
+		*exec = str_concat(head->dir_path, in);
+		if (stat(*exec, &sb) == 0 && sb.st_mode & S_IXUSR)
 			return (1);
-		else if (stat(*exec, &sb) == 0 && input_user[0] != '/')
+		else if (stat(*exec, &sb) == 0)
 		{
 			*status = 126;
 			free(*exec);
@@ -44,14 +44,15 @@ int check_path(char **exec, list_path *head, char *input_user, size_t numwords, 
 		head = head->next;
 		free(*exec);
 	}
-	*exec = input_user;
-	if (stat(input_user, &sb) == 0 && sb.st_mode & S_IXUSR && !S_ISDIR(sb.st_mode) && (input_user[0] == '/' || input_user[0] == '.'))
+	*exec = in;
+	if (stat(in, &sb) == 0 && sb.st_mode & S_IXUSR &&
+	    (in[0] == '/' || in[0] == '.'))
 		return (2);
-	else if (stat(*exec, &sb) == 0 && input_user[0] == '/')
+	else if (stat(*exec, &sb) == 0 && (in[0] == '/' || in[0] == '.'))
 	{
 		*status = 126;
 		return (3);
 	}
 	*status = 127;
-	return (0);
+	return (20);
 }
